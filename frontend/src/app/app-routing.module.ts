169d91e17442d23a0d4822ead4797670e995fbb1
Clemens Emme <Clemens.Emme@gmail.com>
2022-05-20 07:59:54 +0200

    feat(admin): Create Module with Component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {HomeComponent} from './home/home.component';
 import {SettingsComponent} from './settings/settings.component';
 
 const routes: Routes = [
+  {path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule)},
   {path: 'settings', component: SettingsComponent},
   {path: '', component: HomeComponent},
 ];
