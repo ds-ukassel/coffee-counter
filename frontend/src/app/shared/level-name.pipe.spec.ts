22dcffd44a7d39b815a3d6b3a8a19a78fdddbe87
Adrian Kunz <clashsoft@hotmail.com>
2022-06-02 10:30:48 +0200

    refactor(frontend): Add level-name pipe

---
+++ frontend/src/app/shared/level-name.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { LevelNamePipe } from './level-name.pipe';
+
+describe('LevelNamePipe', () => {
+  it('create an instance', () => {
+    const pipe = new LevelNamePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
