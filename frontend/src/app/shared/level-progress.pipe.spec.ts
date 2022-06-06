40598ea2db91a682ff6bb217cb0f777d6934c92b
Clemens Emme <Clemens.Emme@gmail.com>
2022-06-06 11:42:59 +0200

    core: Remove spec files
    Because we are never ever in the time of an eternity use these

--- frontend/src/app/shared/level-progress.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LevelProgressPipe } from './level-progress.pipe';
-
-describe('LevelProgressPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LevelProgressPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
