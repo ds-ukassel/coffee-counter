9e89af22cd480caa299f3ef85672fe448dcea637
Clemens Emme <Clemens.Emme@gmail.com>
2022-06-08 12:27:10 +0200

    feat(user): Add TrophyTier with pipe and interface

--- frontend/src/app/module/user/user.component.ts
+++ frontend/src/app/module/user/user.component.ts
@@ -6,6 +6,8 @@ import {FindAllPurchaseDto, Purchase} from '../../core/model/purchase.interface'
 import {UserService} from '../../core/service/user.service';
 import {CoffeeService} from '../../core/service/coffee.service';
 import {PurchaseService} from '../../core/service/purchase.service';
+import Achievement from '../../core/model/achievement.interface';
+import {TrophyTier} from '../../shared/pipe/trophy-tier.pipe';
 
 @Component({
   selector: 'app-user',
@@ -24,12 +26,12 @@ export class UserComponent implements OnInit {
   price = 0.1;
 
   // TODO rework this unto an db entity
-  achievements = [
+  achievements: Achievement[] = [
     {
       image: '/assets/trophies/nightOwl.svg',
       name: 'Night Owl',
       description: 'Late at night, when everybody is sleeping, it\'s only you, your PC and your coffee.',
-      tier: 'bronze',
+      tier: TrophyTier.bronze,
       hint: 'Register a Coffee between 8PM and 5AM',
       receivedAt: new Date()
     },
@@ -37,7 +39,7 @@ export class UserComponent implements OnInit {
       image: '/assets/trophies/earlyBird.svg',
       name: 'Early Bird',
       description: 'The early bird catches the worm, or as a software engineer would say: "Was eine unheilige Zeit. Erstmal nen Kaffe!"',
-      tier: 'bronze',
+      tier: TrophyTier.bronze,
       hint: 'Register a Coffee between 5AM and 8AM',
       receivedAt: new Date()
     },
@@ -45,7 +47,7 @@ export class UserComponent implements OnInit {
       image: '/assets/trophies/mostAddictedOfThemAll.svg',
       name: 'Most Addicted',
       description: 'One coffee to rule them all, one coffee to find them, One coffee to bring them all, and in the darkness bind them.',
-      tier: 'platinum',
+      tier: TrophyTier.platinum,
       hint: 'Have more coffees registered as any other user',
       receivedAt: new Date()
     },
@@ -53,7 +55,7 @@ export class UserComponent implements OnInit {
       image: '/assets/trophies/buyerOfMilk.svg',
       name: 'Buyer of Milk',
       description: 'Milk is good for your bones! - Skeletor',
-      tier: 'silver',
+      tier: TrophyTier.silver,
       hint: 'Register 5 milk purchases',
       receivedAt: new Date()
     },
@@ -61,7 +63,7 @@ export class UserComponent implements OnInit {
       image: '/assets/trophies/buyerOfNoMilk.svg',
       name: 'Buyer of Non-Milk',
       description: 'By the way, i\'m vegan - Every vegan, ever',
-      tier: 'silver',
+      tier: TrophyTier.silver,
       hint: 'Register 5 milk-alternative purchases',
       receivedAt: new Date()
     },
@@ -69,14 +71,14 @@ export class UserComponent implements OnInit {
       image: '/assets/trophies/buyerOfCoffee.svg',
       name: 'Buyer of Coffee',
       description: 'In germany we call it: "Irgendwer muss ja den Laden am Laufen halten."',
-      tier: 'gold',
+      tier: TrophyTier.gold,
       hint: 'Register 10 coffee purchases',
       receivedAt: new Date()
     }
   ]
 
   // TODO move to own components when clear how achievements are implemented
-  currentAchievement!: { image: string, name: string, description: string, tier: 'bronze' | 'silver' | 'gold' | 'platinum', hint: string, receivedAt: Date };
+  currentAchievement!: Achievement;
   showHint: boolean = false;
 
   constructor(
@@ -104,17 +106,12 @@ export class UserComponent implements OnInit {
     })
   }
 
-  // TODO This implementation is temporary, later-on pure coffees will be replaced by purchases
   createCoffee() {
     this.coffeeService.create({
       userId: this.user._id,
       price: this.price,
     }).subscribe(coffee => {
       this.user.coffees++;
-      this.purchases = [{
-        description: 'One coffee please!',
-        total: -this.price
-      } as Purchase, ...this.purchases.slice(0, 9)];
     });
   }
 
@@ -134,21 +131,5 @@ export class UserComponent implements OnInit {
   open(achievement: any, modal: any) {
     this.currentAchievement = achievement;
     this.modalService.open(modal, {size: 'lg', centered: true}).result.then();
-  }
-
-  trophyStyle(): string {
-    switch (this.currentAchievement.tier) {
-      case 'bronze':
-        return 'text-primary';
-      case 'silver':
-        return 'text-info';
-      case 'gold':
-        return 'text-warning';
-      case 'platinum':
-        return 'text-secondary';
-      default:
-        return 'text-warning';
-    }
-
   }
 }
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {LevelNamePipe} from './pipe/level-name.pipe';
 import {LevelProgressPipe, NextLevelPipe} from './pipe/level-progress.pipe';
 import {LevelPipe} from './pipe/level.pipe';
+import {TrophyTierPipe} from './pipe/trophy-tier.pipe';
 
 const declarations = [
   LevelPipe,
   LevelProgressPipe,
   LevelNamePipe,
   NextLevelPipe,
+  TrophyTierPipe,
 ];
 
 @NgModule({
