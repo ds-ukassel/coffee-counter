fe4d1e95a6a99e93b4a175f41596b1be4b348d9f
Adrian Kunz <clashsoft@hotmail.com>
2022-07-19 12:33:50 +0200

    refactor(frontend): Remove unnecessary coffee array from home

--- frontend/src/app/module/home/home.component.ts
+++ frontend/src/app/module/home/home.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
-import {User} from '../../core/model/user.interface';
-import {Coffee} from '../../core/model/coffee.interface';
-import {UserService} from '../../core/service/user.service';
-import {CoffeeService} from '../../core/service/coffee.service';
 import {NgbOffcanvas} from '@ng-bootstrap/ng-bootstrap';
+import {switchMap} from 'rxjs';
+import {User} from '../../core/model/user.interface';
+import {CoffeeService} from '../../core/service/coffee.service';
+import {UserService} from '../../core/service/user.service';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +14,6 @@ export class HomeComponent implements OnInit {
   @ViewChild('purchaseList') private purchaseList!: TemplateRef<any>;
 
   users: User[] = [];
-  coffees: Coffee[] = [];
   userMap: Record<string, User> = {};
 
   constructor(
@@ -31,7 +30,6 @@ export class HomeComponent implements OnInit {
         this.userMap[user._id] = user;
       }
     });
-    this.coffeeService.findAll().subscribe(coffees => this.coffees = coffees.slice(-10).reverse());
   }
 
   createCoffee(user: User) {
@@ -41,26 +39,17 @@ export class HomeComponent implements OnInit {
     }).subscribe(coffee => {
       user.coffees++;
       user.balance = (+user.balance - coffee.price).toFixed(2);
-      this.coffees = [coffee, ...this.coffees.slice(0, 9)];
-    });
-  }
-
-  deleteCoffee(coffee: Coffee) {
-    this.coffeeService.remove(coffee._id).subscribe(() => {
-      const index = this.coffees.indexOf(coffee);
-      if (index >= 0) {
-        this.coffees.splice(index, 1);
-      }
-      const user = this.userMap[coffee.userId];
-      user && user.coffees--;
     });
   }
 
   deleteLastCoffee(user: User) {
-    const coffee = this.coffees.find(c => c.userId === user._id);
-    if (coffee) {
-      this.deleteCoffee(coffee);
-    }
+    // TODO maybe this can be improved with a better endpoint, e.g. delete many with filter and limit
+    this.coffeeService.findAll({userId: user._id}).pipe(
+      switchMap(coffees => this.coffeeService.remove(coffees[coffees.length - 1]._id)),
+    ).subscribe(coffee => {
+      user.coffees--;
+      user.balance = (+user.balance + coffee.price).toFixed(2);
+    });
   }
 
   openPurchaseList() {
