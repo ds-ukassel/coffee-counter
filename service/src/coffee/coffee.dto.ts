b45cadf6d5f91cbbc05c67d4880619913539fff1
Adrian Kunz <clashsoft@hotmail.com>
2022-07-19 12:29:58 +0200

    feat(service): Find all coffees with filter

--- service/src/coffee/coffee.dto.ts
+++ service/src/coffee/coffee.dto.ts
@@ -1,7 +1,10 @@
-import {OmitType} from '@nestjs/swagger';
+import {OmitType, PartialType, PickType} from '@nestjs/swagger';
 import {Coffee} from './coffee.schema';
 
 export class CreateCoffeeDto extends OmitType(Coffee, [
 	'createdAt',
 ] as const) {
+}
+
+export class FilterCoffeeDto extends PartialType(Coffee) {
 }
