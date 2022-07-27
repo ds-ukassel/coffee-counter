5758f20cb1d8775fde0c181a72d06071e637d154
Adrian Kunz <clashsoft@hotmail.com>
2022-07-27 18:00:12 +0200

    feat(service): Buyer-of-X achievements

--- service/src/achievement/achievement.handler.ts
+++ service/src/achievement/achievement.handler.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@nestjs/common';
 import {OnEvent} from '@nestjs/event-emitter';
 import {Coffee} from '../coffee/coffee.schema';
+import {Purchase} from '../purchase/purchase.schema';
 import {AchievementService} from './achievement.service';
 
 @Injectable()
@@ -18,6 +19,23 @@ export class AchievementHandler {
 		}
 		if (hours >= 5 && hours <= 8) {
 			await this.awardRepeatable(coffee.userId, 'early-bird', coffee.createdAt);
+		}
+	}
+
+	@OnEvent('users.*.purchases.*.created')
+	async onPurchaseCreated(purchase: Purchase) {
+		let desc = purchase.description.toLowerCase();
+		const milk = ['milk', 'milch'];
+		if (milk.some(m => desc.includes(m))) {
+			await this.awardRepeatable(purchase.userId, 'buyer-of-milk', purchase.createdAt);
+		}
+		const coffee = ['coffee', 'kaffee', 'espresso'];
+		if (coffee.some(c => desc.includes(c))) {
+			await this.awardRepeatable(purchase.userId, 'buyer-of-coffee', purchase.createdAt);
+		}
+		const notMilk = ['non-milk', 'nicht-milch', 'alt-milch', 'alt-milk', 'hafermilch', 'oat milk'];
+		if (notMilk.some(x => desc.includes(x))) {
+			await this.awardRepeatable(purchase.userId, 'buyer-of-non-milk', purchase.createdAt);
 		}
 	}
 
