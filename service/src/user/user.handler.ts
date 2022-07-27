8ada0f5e0d02f04230242410bf968b8f2f05cec1
Adrian Kunz <clashsoft@hotmail.com>
2022-07-27 16:46:07 +0200

    refactor(service): Update User coffees using event handler

--- service/src/user/user.handler.ts
+++ service/src/user/user.handler.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@nestjs/common';
 import {OnEvent} from '@nestjs/event-emitter';
 import {Achievement} from '../achievement/achievement.schema';
+import {Coffee} from '../coffee/coffee.schema';
 import {UserService} from './user.service';
 
 @Injectable()
@@ -18,5 +19,25 @@ export class UserHandler {
 	@OnEvent('users.*.achievements.*.deleted')
 	async onAchievementDeleted(achievement: Achievement): Promise<void> {
 		await this.userService.update(achievement.userId, {$inc: {achievements: -1}});
+	}
+
+	@OnEvent('users.*.coffees.*.created')
+	async onCoffeeCreated(coffee: Coffee): Promise<void> {
+		await this.userService.update(coffee.userId, {
+			$inc: {
+				coffees: +1,
+				balance: -coffee.price,
+			},
+		});
+	}
+
+	@OnEvent('users.*.coffees.*.deleted')
+	async onCoffeeDeleted(coffee: Coffee): Promise<void> {
+		await this.userService.update(coffee.userId, {
+			$inc: {
+				coffees: -1,
+				balance: +coffee.price,
+			},
+		});
 	}
 }
