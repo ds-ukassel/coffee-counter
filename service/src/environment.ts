17ad4780051a9206f99188ef3bd11ee4245e2613
Adrian Kunz <clashsoft@hotmail.com>
2022-09-08 10:40:20 +0200

    fix(service): Dev MongoDB URI

--- service/src/environment.ts
+++ service/src/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
 	version: 'v1',
 	port: +(process.env.PORT || 3000),
 	mongo: {
-		uri: process.env.MONGO_URI || 'mongodb://localhost:27017/coffee-counter',
+		uri: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/coffee-counter',
 		options: {},
 	},
 	apiKey: process.env.COFFEE_API_KEY,
