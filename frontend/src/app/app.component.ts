c5431f6ef1a7977aa9fa8b685f2ef1656527bcc8
Adrian Kunz <a.kunz@uni-kassel.de>
2025-01-03 14:42:28 +0100

    chore: Migrate to standalone bootstrap

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import {Component} from '@angular/core';
+import {NavbarComponent} from './navbar/navbar.component';
+import {RouterOutlet} from '@angular/router';
+import {ToastModule} from '@mean-stream/ngbx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone: false,
+  imports: [
+    NavbarComponent,
+    RouterOutlet,
+    ToastModule,
+  ],
 })
 export class AppComponent {
   title = 'frontend';
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,21 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode, importProvidersFrom, isDevMode} from '@angular/core';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {environment} from './environments/environment';
 import {registerLocaleData} from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import localeDeExtra from '@angular/common/locales/extra/de';
-
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {ApiKeyInterceptor} from './app/core/service/api-key.interceptor';
+import {CookieService} from 'ngx-cookie-service';
+import {provideCharts, withDefaultRegisterables} from 'ng2-charts';
+import {BarController, Colors, Legend} from 'chart.js';
+import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app/app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from '@angular/forms';
+import {ToastModule} from '@mean-stream/ngbx';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {AppComponent} from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
@@ -14,5 +23,28 @@ if (environment.production) {
 
 registerLocaleData(localeDe, 'de-DE', localeDeExtra);
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      NgbModule,
+      FormsModule,
+      ToastModule,
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        // Register the ServiceWorker as soon as the application is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000',
+      })),
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: ApiKeyInterceptor,
+    },
+    CookieService,
+    provideHttpClient(withInterceptorsFromDi()),
+    provideCharts(withDefaultRegisterables(BarController, Legend, Colors)),
+  ],
+})
   .catch(err => console.error(err));
