c60a28d9e22c6df901e47f5c199caa5655fab4a5
Adrian Kunz <a.kunz@uni-kassel.de>
2025-08-19 10:44:49 +0200

    feat(frontend): Update balance after create purchase

--- frontend/src/app/shared/purchase-list/purchase-list.component.ts
+++ frontend/src/app/shared/purchase-list/purchase-list.component.ts
@@ -1,8 +1,9 @@
 import {CurrencyPipe, DatePipe} from '@angular/common';
 import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 import {NgbPopover, NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
-import {EMPTY, forkJoin, map, Observable, switchMap} from 'rxjs';
+import {forkJoin, map, Observable, switchMap} from 'rxjs';
+import {Coffee} from '../../core/model/coffee.interface';
 import {Purchase} from '../../core/model/purchase.interface';
 import {User} from '../../core/model/user.interface';
 import {CoffeeService} from '../../core/service/coffee.service';
@@ -36,21 +37,20 @@ export class PurchaseListComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
     private coffeeService: CoffeeService,
     private purchaseService: PurchaseService,
   ) {
   }
 
-  ngOnInit(): void {
-    const purchaseToItem = (p: Purchase) => ({
-      ...p,
-      description: p.description ?? 'Purchase',
-      type: 'purchase' as const,
-      icon: 'bi-shop',
-      money: p.total,
-    });
+  addCoffee(coffee: Coffee) {
+    this.items.unshift(coffeeToItem(coffee));
+  }
 
+  addPurchase(purchase: Purchase) {
+    this.items.unshift(purchaseToItem(purchase));
+  }
+
+  ngOnInit(): void {
     const userId$ = this.route.params.pipe(map(({user}): string => user));
 
     userId$.pipe(
@@ -60,23 +60,10 @@ export class PurchaseListComponent implements OnInit {
       ])),
     ).subscribe(([coffees, purchases]) => {
       this.items = [
-        ...coffees.map(c => ({
-          ...c,
-          description: 'Coffee',
-          type: 'coffee' as const,
-          icon: 'bi-cup',
-          money: -c.price,
-        })),
+        ...coffees.map(coffeeToItem),
         ...purchases.map(purchaseToItem),
       ].sort((a, b) => +new Date(b.createdAt) - +new Date(a.createdAt)).slice(0, 20);
     });
-
-    this.route.queryParams.pipe(
-      switchMap(({newPurchase}) => newPurchase ? this.purchaseService.findOne(newPurchase) : EMPTY),
-    ).subscribe(newPurchase => {
-      this.items.unshift(purchaseToItem(newPurchase));
-      this.router.navigate([], {relativeTo: this.route, queryParams: {newPurchase: null}, queryParamsHandling: 'merge'});
-    })
   }
 
   delete(purchase: Item) {
@@ -84,5 +71,29 @@ export class PurchaseListComponent implements OnInit {
     del.subscribe(res => {
       this.items = this.items.filter(item => item._id !== res._id);
     });
+  }
+}
+
+function coffeeToItem(c: Coffee): Item {
+  return {
+    _id: c._id,
+    createdAt: c.createdAt,
+    userId: c.userId,
+    description: 'Coffee',
+    type: 'coffee' as const,
+    icon: 'bi-cup',
+    money: -c.price,
+  };
+}
+
+function purchaseToItem(p: Purchase): Item {
+  return {
+    _id: p._id,
+    createdAt: p.createdAt,
+    userId: p.userId,
+    description: p.description ?? 'Purchase',
+    type: 'purchase' as const,
+    icon: 'bi-shop',
+    money: p.total,
   }
 }
